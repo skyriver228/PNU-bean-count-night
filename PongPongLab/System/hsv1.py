cee7d804fc1ef14c2c107c84032eb96bac365ce0
skyriver228 <63352107+skyriver228@users.noreply.github.com>
2021-12-01 00:12:05 +0900

    update hsv

---
+++ PongPongLab/System/hsv1.py
@@ -0,0 +1,24 @@
+import sys
+import numpy as np
+import cv2
+
+
+input_img1 = "./Open/t25/1.jpg"
+src = cv2.imread(input_img1)
+#src = cv2.imread('candies2.png')
+
+if src is None:
+    print('Image load failed!')
+    sys.exit()
+
+src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+
+dst1 = cv2.inRange(src, (0, 128, 0), (100, 255, 100))
+dst2 = cv2.inRange(src_hsv, (50, 150, 0), (80, 255, 255))
+
+cv2.imshow('src', src)
+cv2.imshow('dst1', dst1)
+cv2.imshow('dst2', dst2)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
