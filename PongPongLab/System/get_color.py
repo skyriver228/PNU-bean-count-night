e19eab83dad200370e6de98b207e78feaf91d977
opdev1 <opdev1@gg.com>
2021-11-30 13:31:53 +0900

    색상 코드를 찾아서 ...

--- PongPongLab/System/get_color.py
+++ PongPongLab/System/get_color.py
@@ -8,9 +8,6 @@ path = "./capture"
 file_lst = os.listdir(path)
 print(file_lst)
 
-# for file in file_lst:
-#     filepath = path + '/' + file
-
 img = cv2.imread(path + '/' + file_lst[0])
 if img is None:
     print('Image load failed!')
@@ -23,16 +20,26 @@ height, width = input_image.shape[:2]  # 이미지의 높이와 너비 불러옴
 
 img_hsv = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)  # cvtColor 함수를 이용하여 hsv 색공간으로 변환
 
-lower_blue = (120 - 10, 30, 30)  # hsv 이미지에서 바이너리 이미지로 생성 , 적당한 값 30
-upper_blue = (120 + 10, 255, 255)
-img_mask = cv2.inRange(img_hsv, lower_blue, upper_blue)  # 범위내의 픽셀들은 흰색, 나머지 검은색
+dst1 = cv2.inRange(input_image, (0, 128, 0), (100, 255, 100))
+dst2 = cv2.inRange(img_hsv, (50, 150, 0), (80, 255, 255))
 
-# 바이너리 이미지를 마스크로 사용하여 원본이미지에서 범위값에 해당하는 영상부분을 획득
-img_result = cv2.bitwise_and(input_image, input_image, mask=img_mask)
+cv2.imshow('src', input_image)
+cv2.imshow('dst1', dst1)
+cv2.imshow('dst2', dst2)
+cv2.waitKey()
 
-cv2.imshow('img_origin', input_image)
-cv2.imshow('img_mask', img_mask)
-cv2.imshow('img_color', img_result)
-
-cv2.waitKey(0)
 cv2.destroyAllWindows()
+#
+# lower_color = (21 - 10, 30, 30)  # hsv 이미지에서 바이너리 이미지로 생성 , 적당한 값 30
+# upper_color = (21 + 10, 255, 255)
+# img_mask = cv2.inRange(img_hsv, lower_color, upper_color)  # 범위내의 픽셀들은 흰색, 나머지 검은색
+#
+# # 바이너리 이미지를 마스크로 사용하여 원본이미지에서 범위값에 해당하는 영상부분을 획득
+# img_result = cv2.bitwise_and(input_image, input_image, mask=img_mask)
+#
+# cv2.imshow('img_origin', input_image)
+# cv2.imshow('img_mask', img_mask)
+# cv2.imshow('img_color', img_result)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
