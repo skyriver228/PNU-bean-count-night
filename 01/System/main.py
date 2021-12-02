ba3696b1b7a6c5111ddb3ea601f0e446a79c069b
skyriver228 <skyriver0822@gmail.com>
2021-12-02 14:59:26 +0900

    update getImagePath

--- 01/System/main.py
+++ 01/System/main.py
@@ -5,12 +5,13 @@ class BeanCount:
     def __init__(self, path: str):
         # Hidden or Open
         self.path = path
-        self.image_path_list = self.getImagePath(path)
+        self.image_path_list = []
 
     def getImagePath(self):
-        option = int(input("input number of target"))
-        test_l = [f"/t{i:02d}" for i in range(10, 31)]
+        option = [int(i) for i in input("input number of target __.jpg(if you want './Open/t01/5.jpg' ~ './Open/t30/3.jpg', input: 1 2 3)").split()]
+        test_l = [f"/t{i:02d}" for i in range(1, 31)]
         image_l = [f"/{i}.jpg" for i in range(1,6)]
 
         test_path_l = [self.path+i for i in test_l]
-        f_l_5 = [i+image_l[4] for i in test_path_l]
+        for i in option:
+            self.image_path_list.extend([i+image_l[i-1] for i in test_path_l])
