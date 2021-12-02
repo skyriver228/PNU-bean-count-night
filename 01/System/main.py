b9a423f441fc416643b5811941c864609465fa8b
skyriver228 <skyriver0822@gmail.com>
2021-12-02 16:15:26 +0900

    done with main.py

--- 01/System/main.py
+++ 01/System/main.py
@@ -6,6 +6,7 @@ class BeanCount:
     def __init__(self, export_file_path: str):
         self.test_l = [f"/t{i:02d}" for i in range(1, 31)]
         self.image_l = [f"/{i}.jpg" for i in range(1,6)]
+        self.option = 0
         self.image_path_list = []
         self.count_label_path = "./01/System/count.txt"
         self.start_time = 0
@@ -16,6 +17,7 @@ class BeanCount:
 
 
     def countingBean(self):
+        self.option = int(input("input number of target __.jpg(select one from 1,2,3,4,5: "))
         self.start_time = dt.datetime.now()
         o_image_path_list = self.getImagePath("./Open")
         o_image_pixel_count_list = self.get_above_area(o_image_path_list)
@@ -28,14 +30,13 @@ class BeanCount:
     # path = "./Open" or "./Hidden"
     def getImagePath(self, path):
         image_path_list = []
-        option = int(input("input number of target __.jpg(select one from 1,2,3,4,5: "))
 
         test_path_l = [path+i for i in self.test_l]
-        image_path_list.extend([j+self.image_l[option-1] for j in test_path_l])
+        image_path_list.extend([j+self.image_l[self.option-1] for j in test_path_l])
         return image_path_list
 
 
-    def get_above_area(image_path_list):
+    def get_above_area(self, image_path_list):
         image_pixel_count_list = []
         for img_path in image_path_list:
             src=cv2.imread(img_path)
@@ -69,13 +70,15 @@ class BeanCount:
 
 
     def modelResult(self):
-        h_image_path_list = self.getImagePath(self, "./Hidden")
+        # Hidden data를 제공받지 않아 일단 Open으로 진행
+        # h_image_path_list = self.getImagePath("./Hidden")
+        h_image_path_list = self.getImagePath("./Open")
         h_image_pixel_count_list = self.get_above_area(h_image_path_list)
         self.count_res = self.linear_model_fn(h_image_pixel_count_list)
 
 
     def exportingOutput(self):
-        f = open(self.export_file, 'w')
+        f = open(self.export_file_path, 'w')
         f.write("Team  PongPongLab\n")
 
         s_t = self.start_time
@@ -87,10 +90,10 @@ class BeanCount:
         t = "Time  "+str(diff.seconds)+"\n"
         f.write(t)
 
-        c = "Cases "+str(len(self.image_path_list))+"\n"
+        res = self.count_res
+        c = "Cases "+str(len(res))+"\n"
         f.write(c)
 
-        res = self.count_res
         for i in range(len(res)):
             data = res[i]
             f_d = str(self.test_l[i][1:])+"  "+str(round(data,3))+"\n"
