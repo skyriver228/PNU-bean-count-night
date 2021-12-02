ee47c2c2c1272311f2c5ab3703c9588368ce3d3a
skyriver228 <skyriver0822@gmail.com>
2021-12-02 16:08:05 +0900

    updating main.py almost done

--- 01/System/main.py
+++ 01/System/main.py
@@ -1,62 +1,105 @@
 import numpy as np
 import cv2
-import time
 import datetime as dt
 
 class BeanCount:
-    def __init__(self, path: str):
-        # Hidden or Open
-        self.path = path
+    def __init__(self, export_file_path: str):
+        self.test_l = [f"/t{i:02d}" for i in range(1, 31)]
+        self.image_l = [f"/{i}.jpg" for i in range(1,6)]
         self.image_path_list = []
-        self.
+        self.count_label_path = "./01/System/count.txt"
+        self.start_time = 0
+        self.end_time = 0
+        self.count_res = []
+        self.linear_model_fn = None
+        self.export_file_path = export_file_path
 
-    def getImagePath(self):
-        option = [int(i) for i in input("input number of target __.jpg(if you want './Open/t01/5.jpg' ~ './Open/t30/3.jpg', input: 1 2 3)").split()]
-        test_l = [f"/t{i:02d}" for i in range(1, 31)]
-        image_l = [f"/{i}.jpg" for i in range(1,6)]
 
-        test_path_l = [self.path+i for i in test_l]
-        for i in option:
-            self.image_path_list.extend([j+image_l[i-1] for j in test_path_l])
+    def countingBean(self):
+        self.start_time = dt.datetime.now()
+        o_image_path_list = self.getImagePath("./Open")
+        o_image_pixel_count_list = self.get_above_area(o_image_path_list)
+        self.fittingModel(o_image_pixel_count_list, self.count_label_path)
+        self.modelResult()
+        self.end_time = dt.datetime.now()
+        self.exportingOutput()
+         
 
-    def get_above_area(img_path, res_l):
-        src=cv2.imread(img_path)
-        rc = (975, 909, 2555, 999)
-        src = src[rc[1]:rc[1]+rc[3], rc[0]:rc[0]+rc[2]]
-        dst = src
-        src_hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
+    # path = "./Open" or "./Hidden"
+    def getImagePath(self, path):
+        image_path_list = []
+        option = int(input("input number of target __.jpg(select one from 1,2,3,4,5: "))
 
-            # dst1 = cv2.inRange(src, (0, 128, 0), (100, 255, 100))
-        dst1 = cv2.inRange(src_hsv, (16, 13, 0), (80, 250, 255))
-        dst2 = cv2.morphologyEx(dst1, cv2.MORPH_OPEN, None)
+        test_path_l = [path+i for i in self.test_l]
+        image_path_list.extend([j+self.image_l[option-1] for j in test_path_l])
+        return image_path_list
 
-        kernel = np.ones((6, 6), np.uint8)
-        closing = cv2.morphologyEx(dst2, cv2.MORPH_CLOSE,kernel, iterations = 15)
-        bg = cv2.dilate(closing, kernel, iterations = 1)
-        dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 0)
-        ret, fg = cv2.threshold(dist_transform, 0.02*dist_transform.max(), 255, 0)
-        res_l.append(sum(sum(fg)))
+
+    def get_above_area(image_path_list):
+        image_pixel_count_list = []
+        for img_path in image_path_list:
+            src=cv2.imread(img_path)
+            rc = (975, 909, 2555, 999)
+            src = src[rc[1]:rc[1]+rc[3], rc[0]:rc[0]+rc[2]]
+            dst = src
+            src_hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
+
+                # dst1 = cv2.inRange(src, (0, 128, 0), (100, 255, 100))
+            dst1 = cv2.inRange(src_hsv, (16, 13, 0), (80, 250, 255))
+            dst2 = cv2.morphologyEx(dst1, cv2.MORPH_OPEN, None)
+
+            kernel = np.ones((6, 6), np.uint8)
+            closing = cv2.morphologyEx(dst2, cv2.MORPH_CLOSE,kernel, iterations = 15)
+            bg = cv2.dilate(closing, kernel, iterations = 1)
+            dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 0)
+            ret, fg = cv2.threshold(dist_transform, 0.02*dist_transform.max(), 255, 0)
+            image_pixel_count_list.append(sum(sum(fg)))
+        return image_pixel_count_list
+
 
     def fittingModel(self, x, y):
-        x = res_l_5
+        f = open(y, 'r')
+        line = f.readline()
+        count = line.split()
+        f.close()
         y = [int(i) for i in count]
 
         linear_model=np.polyfit(x,y,3)
-        linear_model_fn=np.poly1d(linear_model)
+        self.linear_model_fn=np.poly1d(linear_model)
 
-        return linear_model_fn
 
-    def exportingOutput():
-        f = open("./01/Out/Kong_01.txt", 'w')
+    def modelResult(self):
+        h_image_path_list = self.getImagePath(self, "./Hidden")
+        h_image_pixel_count_list = self.get_above_area(h_image_path_list)
+        self.count_res = self.linear_model_fn(h_image_pixel_count_list)
+
+
+    def exportingOutput(self):
+        f = open(self.export_file, 'w')
         f.write("Team  PongPongLab\n")
-        date = "Date  " + f"{d.month}-{d.day}-{d.hour}-{d.minute}-{d.second}"+"\n"
+
+        s_t = self.start_time
+        date = "Date  " + f"{s_t.month}-{s_t.day}-{s_t.hour}-{s_t.minute}-{s_t.second}"+"\n"
         f.write(date)
-        t = "Time  "+str(round(time.time() - start,2))+"\n"
+        
+        e_t = self.end_time
+        diff = s_t - e_t
+        t = "Time  "+str(diff.seconds)+"\n"
         f.write(t)
-        c = "Cases "+str(len(res_l_5))+"\n"
+
+        c = "Cases "+str(len(self.image_path_list))+"\n"
         f.write(c)
-        for i in range(len(y_new)):
-            data = y_new[i]
-            f_d = str(f2_l[i][1:])+"  "+str(round(data,3))+"\n"
+
+        res = self.count_res
+        for i in range(len(res)):
+            data = res[i]
+            f_d = str(self.test_l[i][1:])+"  "+str(round(data,3))+"\n"
             f.write(f_d)
         f.close()
+
+def main():
+    bc = BeanCount("./01/Out/Kong_01.txt")
+    bc.countingBean()
+
+if __name__ == "__main__":
+    main()
